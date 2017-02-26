a7deed8172398f481cca8790c427dc17a168b3e2
doczal <doczal@gmail.com>
2017-02-26 13:20:20 +0800

    Fixed css background not showing

--- gulpfile.js
+++ gulpfile.js
@@ -71,8 +71,8 @@ gulp.task('sass-prod', function () {
   .pipe(sass())
   .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
   .pipe(cssnano())
-  .pipe(gulp.dest('_site/css'));
-  //.pipe(gulp.dest('css'));
+  .pipe(gulp.dest('_site/css'))
+  .pipe(gulp.dest('assets/css'));
 });
 
 /*
